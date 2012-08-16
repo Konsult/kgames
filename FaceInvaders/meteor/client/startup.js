6d234571fdbe1f8120ea720d947cbdce3bf14252
Jing Jin <jing@konsu.lt>
2012-08-16 16:46:03 -0700

    Make the game reasonably-sized on the iPhone.

--- FaceInvaders/meteor/client/startup.js
+++ FaceInvaders/meteor/client/startup.js
@@ -3,6 +3,16 @@
 Meteor.startup(function () {
   Backbone.history.start({pushState: true});
 
+  var optimalWidth = 1440;
+  var deviceWidth = $(window).width();
+  if (deviceWidth < optimalWidth) {
+    // iPhone chokes on anything > 3 when going portrait -> landscape -> portrait.
+    var deviceWidthRatio = 3;
+    var desiredWidth = Math.min(deviceWidthRatio * deviceWidth, optimalWidth);
+    var scale = (deviceWidth / desiredWidth).toFixed(2);
+    $("meta[name=viewport]").attr("content", "width=" + desiredWidth + ", initial-scale=" + scale + ", maximum-scale=" + scale + ", user-scalable=no");
+  }
+
   var el = $(document.body);
   window.app = new Game(el);
 });
