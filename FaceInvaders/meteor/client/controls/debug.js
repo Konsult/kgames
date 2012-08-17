f2de1cb0650a8fd4f1c92b20d81fb1b0d9094711
Matthew Delaney <mdelaney7@gmail.com>
2012-08-17 16:36:33 -0700

    Added in FPS to debug menu.

--- FaceInvaders/meteor/client/controls/debug.js
+++ FaceInvaders/meteor/client/controls/debug.js
@@ -2,22 +2,35 @@ function DebugDisplay () {
   this.el = $("<div class='DebugDisplay'>");
   this.isShown = false;
   this.el.css("display", "none");
-  this.el.append("Debug Info here");
+  this.text = $("<p>").html("Debug Info here");
+  this.el.append(this.text);
+
+  // Refresh Interval ID
+  this.intervalID = null;
+  this.interval = 500;
 }
 
 DebugDisplay.prototype.toggle = function () {
-  if (this.isShown)
-    this.hide();
-  else
-    this.show();
+  if (this.isShown) this.hide();
+  else this.show();
 }
 
 DebugDisplay.prototype.show = function () {
   this.el.css("display", "block");
   this.isShown = true;
+
+  // Kick of Refresh Interval
+  var that = this;
+  this.intervalID = setInterval(function () {
+    var fps = Stats.getStat("frame", "eps");
+    fps = Math.round(fps * 10) / 10;
+    var str = fps+" FPS";
+    that.text.html(str);
+  }, this.interval);
 }
 
 DebugDisplay.prototype.hide = function () {
   this.el.css("display", "none");
   this.isShown = false;
+  clearInterval(this.intervalID);
 }
